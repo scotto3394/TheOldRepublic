96a639d1624351824cccfe1536de37084998598c
Scott Manifold <shm.qed@gmail.com>
2017-08-30 04:57:59 -0700

    The smallest of baby steps towards graphics. Introducing Piston

--- src/bin_client.rs
+++ src/bin_client.rs
@@ -2,12 +2,13 @@
 extern crate httk;
 
 //Inside Libraries
-use httk::network::client::start_connection;
+// use httk::network::client::start_connection;
+use httk::graphics::draw_rectangle;
 
 //External Libraries
 
 fn main() {
-
-	let address = "coruscant.smanifold.com:80";
-	start_connection(address);
+	// let address = "coruscant.smanifold.com:80";
+	// start_connection(address);
+	draw_rectangle();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,9 @@ extern crate tokio_io;
 extern crate tokio_proto;
 extern crate tokio_service;
 
+extern crate piston_window;
+
 mod combat;
 mod core;
+pub mod graphics;
 pub mod network;
